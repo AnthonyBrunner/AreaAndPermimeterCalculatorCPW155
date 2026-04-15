39ff200fda6c696aafce5a993c124e49a544a581
AnthonyBrunner <abrunner5361@students.cptc.edu>
2026-04-15 16:01:21 -0700

    Added required controls to the form and got them aligned. Also updated text and (Name) for all

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            calculateButton = new Button();
+            lengthText = new TextBox();
+            widthText = new TextBox();
+            areaText = new TextBox();
+            lengthLabel = new Label();
+            widthLabel = new Label();
+            areaLabel = new Label();
+            perimeterText = new TextBox();
+            perimeterLabel = new Label();
+            SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            calculateButton.Location = new Point(46, 195);
+            calculateButton.Name = "calculateButton";
+            calculateButton.Size = new Size(118, 59);
+            calculateButton.TabIndex = 0;
+            calculateButton.Text = "Calculate Area and Perimeter";
+            calculateButton.UseVisualStyleBackColor = true;
+            // 
+            // lengthText
+            // 
+            lengthText.Location = new Point(95, 33);
+            lengthText.Name = "lengthText";
+            lengthText.Size = new Size(100, 23);
+            lengthText.TabIndex = 1;
+            // 
+            // widthText
+            // 
+            widthText.Location = new Point(95, 70);
+            widthText.Name = "widthText";
+            widthText.Size = new Size(100, 23);
+            widthText.TabIndex = 2;
+            // 
+            // areaText
+            // 
+            areaText.Location = new Point(95, 109);
+            areaText.Name = "areaText";
+            areaText.Size = new Size(100, 23);
+            areaText.TabIndex = 3;
+            // 
+            // lengthLabel
+            // 
+            lengthLabel.AutoSize = true;
+            lengthLabel.Location = new Point(31, 41);
+            lengthLabel.Name = "lengthLabel";
+            lengthLabel.Size = new Size(44, 15);
+            lengthLabel.TabIndex = 4;
+            lengthLabel.Text = "Length";
+            // 
+            // widthLabel
+            // 
+            widthLabel.AutoSize = true;
+            widthLabel.Location = new Point(36, 78);
+            widthLabel.Name = "widthLabel";
+            widthLabel.Size = new Size(39, 15);
+            widthLabel.TabIndex = 5;
+            widthLabel.Text = "Width";
+            // 
+            // areaLabel
+            // 
+            areaLabel.AutoSize = true;
+            areaLabel.Location = new Point(39, 117);
+            areaLabel.Name = "areaLabel";
+            areaLabel.Size = new Size(31, 15);
+            areaLabel.TabIndex = 6;
+            areaLabel.Text = "Area";
+            // 
+            // perimeterText
+            // 
+            perimeterText.Location = new Point(95, 150);
+            perimeterText.Name = "perimeterText";
+            perimeterText.Size = new Size(100, 23);
+            perimeterText.TabIndex = 7;
+            // 
+            // perimeterLabel
+            // 
+            perimeterLabel.AutoSize = true;
+            perimeterLabel.Location = new Point(12, 158);
+            perimeterLabel.Name = "perimeterLabel";
+            perimeterLabel.Size = new Size(58, 15);
+            perimeterLabel.TabIndex = 8;
+            perimeterLabel.Text = "Perimeter";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(216, 292);
+            Controls.Add(perimeterLabel);
+            Controls.Add(perimeterText);
+            Controls.Add(areaLabel);
+            Controls.Add(widthLabel);
+            Controls.Add(lengthLabel);
+            Controls.Add(areaText);
+            Controls.Add(widthText);
+            Controls.Add(lengthText);
+            Controls.Add(calculateButton);
+            Name = "Form1";
+            Text = "Area and Perimeter Calculator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button calculateButton;
+        private TextBox lengthText;
+        private TextBox widthText;
+        private TextBox areaText;
+        private Label lengthLabel;
+        private Label widthLabel;
+        private Label areaLabel;
+        private TextBox perimeterText;
+        private Label perimeterLabel;
     }
 }
