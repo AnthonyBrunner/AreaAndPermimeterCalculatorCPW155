4ac942ff26ecd8ec1ffd20c7632783bbb7f7a41f
AnthonyBrunner <abrunner5361@students.cptc.edu>
2026-04-15 16:14:28 -0700

    Wrote in the logic for the Form to operate as expected for the base requirements

--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,23 @@ namespace AreaAndPermimeterCalculator
         private void calculateButton_Click(object sender, EventArgs e)
         {
             // Get user input from the Length and Width Text box
+            string inputLength = lengthText.Text;
+            string inputWidth = widthText.Text;
 
             // Convert data to an integer
+            int length = Convert.ToInt32(inputLength);
+            int width = Convert.ToInt32(inputWidth);
 
             // Calculate the Area
-
+            int area = length * width;
+            
             // Calculate the Perimeter
+            // The formula for perimeter of a rectangle (2 * width) + (2 * length)
+            int perimeter = (2 * length) + (2 * width);       
 
             // Display results to the user
+            areaText.Text = area.ToString();
+            perimeterText.Text = perimeter.ToString();
         }
     }
 }
